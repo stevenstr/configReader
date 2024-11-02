4a91e02108e5e065970284e76cef48e8c9858c15
SS <stephenzhavrid@gmail.com>
2024-11-02 12:33:38 +0300

    read configs using json module

--- main.go
+++ main.go
@@ -1,31 +1,70 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
-
-	"github.com/spf13/viper"
+	"io"
+	"os"
 )
 
 const (
 	configFile = "config.json"
-	configType = "json"
+	sep        = '\n'
 )
 
 type ConfigStructure struct {
-	Linux        string `mapstructure:"linux"`
-	Windows      string `mapstructure:"windows"`
-	Active       bool   `mapstructure:"active"`
-	Postgres     string `mapstructure:"postgres"`
-	PostgresUser string `mapstructure:"postgresUser"`
-	PostgresPort int    `mapstructure:"postgresPort"`
-	Mongodb      string `mapstructure:"mongodb"`
-	MongodbUser  string `mapstructure:"mongodbUser"`
-	MongodbPort  int    `mapstructure:"mongodbPort"`
+	Linux        string `json:"linux"`
+	Windows      string `json:"windows"`
+	Active       bool   `json:"active"`
+	Postgres     string `json:"postgres"`
+	PostgresUser string `json:"postgresUser"`
+	PostgresPort string `json:"postgresPort"`
+	Mongodb      string `json:"mongodb"`
+	MongodbUser  string `json:"mongodbUser"`
+	MongodbPort  string `json:"mongodbPort"`
+}
+
+func ParseConfigs() (ConfigStructure, error) {
+	stream, err := os.Open(configFile)
+	if err != nil {
+		return ConfigStructure{}, err
+	}
+	defer stream.Close()
+
+	reader := bufio.NewReader(stream)
+
+	resString := ""
+	for {
+		line, err := reader.ReadString(sep)
+		if err == io.EOF {
+			resString += line
+			break
+		}
+		if err != nil {
+			break
+		}
+		resString += line
+	}
+
+	fmt.Println("Try to unmarshal configs...")
+	configs := ConfigStructure{}
+	err = json.Unmarshal([]byte(resString), &configs)
+	if err != nil {
+		return ConfigStructure{}, err
+	}
+
+	return configs, err
 }
 
 func main() {
-	viper.SetConfigType(configType)
-	viper.SetConfigFile(configFile)
+	myConfigs, err := ParseConfigs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	fmt.Println("config file - ", viper.ConfigFileUsed(), " - was used.")
+	fmt.Println("We recieved this structure:")
+	fmt.Println(myConfigs)
+
 }
