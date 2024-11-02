02794bb67594495b02dd2d62301f71f29b4ac5f0
SS <stephenzhavrid@gmail.com>
2024-11-02 15:36:08 +0300

    was improved errors processing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/viper"
 )
@@ -10,26 +11,39 @@ const (
 	configFile = "config.json"
 	configType = "json"
 	sep        = '\n'
+	prefix     = ""
+	indent     = "\t"
 )
 
 type ConfigStructure struct {
-	Linux        string `json:"linux"`
-	Windows      string `json:"windows"`
-	Active       bool   `json:"active"`
-	Postgres     string `json:"postgres"`
-	PostgresUser string `json:"postgresUser"`
-	PostgresPort string `json:"postgresPort"`
-	Mongodb      string `json:"mongodb"`
-	MongodbUser  string `json:"mongodbUser"`
-	MongodbPort  string `json:"mongodbPort"`
+	Linux        string `mapstructure:"linux"`
+	Windows      string `mapstructure:"windows"`
+	Active       bool   `mapstructure:"active"`
+	Postgres     string `mapstructure:"postgres"`
+	PostgresUser string `mapstructure:"postgresUser"`
+	PostgresPort string `mapstructure:"postgresPort"`
+	Mongodb      string `mapstructure:"mongodb"`
+	MongodbUser  string `mapstructure:"mongodbUser"`
+	MongodbPort  string `mapstructure:"mongodbPort"`
 }
 
 func main() {
 	viper.SetConfigFile(configFile)
 	viper.SetConfigType(configType)
-	viper.ReadInConfig()
 
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("try to unmarshal")
 	var cfgData ConfigStructure
 	viper.Unmarshal(&cfgData)
-	fmt.Println(cfgData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	fmt.Println("go structure was recieved:")
+	fmt.Printf("%#v\n", cfgData)
 }
