d6148349c3e8acd28bc3158f421e5d0d212778da
SS <stephenzhavrid@gmail.com>
2024-11-02 14:54:16 +0300

    viper makes config process really easy!

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 const (
 	configFile = "config.json"
@@ -20,50 +24,14 @@ type ConfigStructure struct {
 	MongodbPort  string `json:"mongodbPort"`
 }
 
-// func ParseConfigs() (ConfigStructure, error) {
-// 	stream, err := os.Open(configFile)
-// 	if err != nil {
-// 		return ConfigStructure{}, err
-// 	}
-// 	defer stream.Close()
-
-// 	reader := bufio.NewReader(stream)
-
-// 	resString := ""
-// 	for {
-// 		line, err := reader.ReadString(sep)
-// 		if err == io.EOF {
-// 			resString += line
-// 			break
-// 		}
-// 		if err != nil {
-// 			break
-// 		}
-// 		resString += line
-// 	}
-
-// 	fmt.Println("Try to unmarshal configs...")
-// 	configs := ConfigStructure{}
-// 	err = json.Unmarshal([]byte(resString), &configs)
-// 	if err != nil {
-// 		return ConfigStructure{}, err
-// 	}
-
-// 	return configs, err
-// }
-
 func main() {
-	// myConfigs, err := ParseConfigs()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// fmt.Println("We recieved this structure:")
-	// fmt.Println(myConfigs)
-
 	viper.SetConfigFile(configFile)
 	viper.SetConfigType(configType)
 	viper.ReadInConfig()
+
+	cfgM := viper.AllSettings()
+	for k, v := range cfgM {
+		fmt.Println(k, ":", v)
+	}
 
 }
